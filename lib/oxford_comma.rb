4fecd308e0294180c94554b68fb0c883293867a0
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-01 14:04:47 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,5 +4,5 @@ def oxford_comma(array)
   array.join(", ")
 end
 
-my_arr = ["fiddleheads","okra","kohlrabi"]
-puts oxford_comma(my_arr)
+# my_arr = ["fiddleheads","okra","kohlrabi"]
+# puts oxford_comma(my_arr)
